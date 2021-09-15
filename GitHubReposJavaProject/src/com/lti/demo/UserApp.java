a333fe1cfbbb02e9bb0da8bbd874cdf0e09b0bd9
kirk910 <kirkrodrigues910@gmail.com>
2021-09-15 16:01:54 +0530

    First Commit

--- GitHubReposJavaProject/src/com/lti/demo/UserApp.java
+++ GitHubReposJavaProject/src/com/lti/demo/UserApp.java
@@ -2,7 +2,7 @@ package com.lti.demo;
 
 public class UserApp {
  public static void main(String[] args) {
-	 Product p1 = new Product(101,"Mobile",5000);
-	 System.out.println(p1);
+	 Product p = new Product(101,"Mobile",5000);
+	 System.out.println(p);
 }
 }
